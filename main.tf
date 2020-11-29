a3a57cf371f322be80bf3f3d7f47975fb7e42a5a
msavita-cloud <65045244+msavita-cloud@users.noreply.github.com>
2020-11-28 23:54:32 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,18 @@
-resource "random_string" "uid" {
-    special = false
-    upper = false
-    length = 6
-}
-
-resource "azurerm_resource_group" "rg-demo" {
-  name     = "rg-${var.namespace}-demo"
-  location = var.location
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg-demo.name
-  location                 = azurerm_resource_group.rg-demo.location
-  account_tier             = var.storage_account_tier
-  account_replication_type = var.account_replication_type
-
-  tags = {
-    environment = var.environment
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
   }
 }
 
-output "resource_group_name" {
-  value = azurerm_storage_account.example.resource_group_name
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
 }
