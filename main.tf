c2e1319444359664cf0777d1f6fd4d0acce9f890
msavita-cloud <65045244+msavita-cloud@users.noreply.github.com>
2020-11-29 00:28:56 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,38 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.26"
-    }
-  }
-}
-
 provider "azurerm" {
-  features {}
+# The "feature" block is required for AzureRM provider 2.x.
+# If you are using version 1.x, the "features" block is not allowed.
+version = "~>2.0"
+
+   subscription_id = "00000000-0000-0000-0000-000000000000"
+   tenant_id = "00000000-0000-0000-0000-000000000000"
+   client_id = “${data.vault_generic_secret.azure.data[“client_id”]}”
+   client_secret = “${data.vault_generic_secret.azure.data[“client_secret”]}”
+
+   features {}
+   }
+
+provider “vault” {
+   address = var.vault_addr
+   auth_login {
+      path = “azure\\creds\\Azure-Terraform”
+      parameters = {
+      role_id   = "00000000-0000-0000-0000-0000000000"
+      #var.login_approle_role_id
+      secret_id = <your approle secret ID here>
+      #var.login_approle_secret_id
+      }    
+   }
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "westus2"
+data “vault_generic_secret” “azure” {
+   path = “azure\\creds\\Azure-Terraform”
 }
+
+module "vpc" {
+   source = "C:\\Amazic\\Azure\\Dev\\Modules\\VPC"
+   cluster_name = "Stage-LampStack"
+   }
+
+module "lamp-stack" {
+   source = " C:\\Amazic\\Azure\\Dev\\Modules\\lamp"
+   }
